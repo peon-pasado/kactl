d087dd6f0401d68aef9ac962d45cbc14633dfef0
Miguel Alessandro Mini H <45865868+peon-pasado@users.noreply.github.com>
2020-04-30 06:38:58 -0500

    Update Dinic2.cpp

--- content/graph/Dinic2.cpp
+++ content/graph/Dinic2.cpp
@@ -5,7 +5,7 @@
 #define trav(i, x) for (auto i : x)
 #define re(x, y, z) for (int x=y; x<z; ++x)
 #define eb emplace_back
-#define fillall(x, v) fill(all(x), v)
+#define set_to(x, v) fill(all(x), v)
 using namespace std;
 using ll = long long;
 using vi = vector<int>;
@@ -30,7 +30,7 @@ struct Dinic {
 
 	bool bfs(int src, int snk) {
 		queue<int> q({src});
-		fillall(d, inf);
+		set_to(d, inf);
 		d[src] = 0;
 		while (!q.empty()) {
 			int v = q.front(); q.pop();
@@ -65,7 +65,7 @@ struct Dinic {
 	ll solve(int s, int t) {
 		ll res = 0;
 		while (bfs(s, t)) {
-			fillall(pt, 0);
+			set_to(pt, 0);
 			while (int flow = dfs(s, t)) {
 				res += flow;
 			}
