3c3921dbdb26d5b0893bb17b8a689bc2cdc99767
Miguel Alessandro Mini H <45865868+peon-pasado@users.noreply.github.com>
2020-04-30 05:58:33 -0500

    min cost max flow implementation

---
+++ content/graph/mcmf.h
@@ -0,0 +1,65 @@
+struct minCostMaxFlow {
+    struct Edge {
+        int to, cap, flow, cost, link;
+        Edge() {}
+        Edge(int to, int cap, int cost, int link, int flow=0):
+                to(to), cap(cap), link(link), flow(flow), cost(cost) {}
+    };
+    vector<vector<Edge>> g;
+    vector<int> d, p;
+ 
+    minCostMaxFlow(int n): g(n), d(n), p(n) {}
+ 
+    void addEdge(int a, int b, int cost, int cap) {
+        if (a == b) return;
+        int pa = sz(g[a]), pb = sz(g[b]);
+        g[a].eb(b, cap, cost, pb); g[b].eb(a, 0, -cost, pa);
+    }
+ 
+    bool spfa(int src, int snk) {
+        set_to(d, inf); set_to(p, -1);
+        priority_queue<ii, vector<ii>, greater<ii>> Q;
+        Q.push({d[src] = 0, src});
+        while (!Q.empty()) {
+            auto q = Q.top(); Q.pop();
+            int x = q.second;
+            int w = q.first;
+            if (x == snk) return 1;
+            if (d[x] < w) continue;
+            re(i, 0, sz(g[x])) {
+                auto e = g[x][i];
+                if (e.flow < e.cap and w + e.cost < d[e.to]) {
+                    d[e.to] = w + e.cost;
+                    p[e.to] = e.link;
+                    Q.push({d[e.to], e.to});
+                }
+            }
+        }
+        return 0;
+    }
+ 
+    int solve(int s, int t, int k) {
+        int flow = 0, cost = 0;
+        while (flow < k) {
+            if (!spfa(s, t)) break;
+            int f = k - flow;
+            int cur = t;
+            while (cur != s) {
+                auto e = g[cur][p[cur]];
+                int push = g[e.to][e.link].cap - g[e.to][e.link].flow;
+                f = min(f, push);
+                cur = e.to;
+            }
+            flow += f;
+            cost += f * d[t];
+            cur = t;
+            while (cur != s) {
+                auto& e = g[cur][p[cur]];
+                e.flow -= f;
+                g[e.to][e.link].flow += f;
+                cur = e.to;
+            }
+        }
+        return flow < k ? -1 : cost;
+    }
+};
