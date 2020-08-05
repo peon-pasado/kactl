68cf13b3cac830979e047420f0760d86b9533c40
Miguel Alessandro Mini H <45865868+peon-pasado@users.noreply.github.com>
2020-08-05 02:25:12 -0500

    Update minQueue.h

--- content/data-structures/minQueue.h
+++ content/data-structures/minQueue.h
@@ -1,14 +1,14 @@
 template <class T>
 struct minQueue {
-	stack<T> l, r;
+  stack<T> l, r;
   T right_min;
   minQueue(): right_min(1e18) {}
   void add(T c) {
     r.push(c);
     right_min = min(right_min, c);
   }
-	void pop() {
-		if (l.empty()) {
+  void pop() {
+    if (l.empty()) {
       right_min = 1e18;
       while (!r.empty()) {
         if (l.empty()) l.push(r.top());
@@ -17,9 +17,9 @@ struct minQueue {
       }
       if (!l.empty()) l.pop();
     } else l.pop();
-	}
-	T get_min() {
-		if (l.empty()) return right_min;
+  }
+  T get_min() {
+    if (l.empty()) return right_min;
     return min(l.top(), right_min);
-	}
+  }
 };
