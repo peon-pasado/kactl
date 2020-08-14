436b9d59a74c511f3229b90ec81fa28addc349a5
Miguel Alessandro Mini H <45865868+peon-pasado@users.noreply.github.com>
2020-08-14 12:10:47 -0500

    Update optQueue.h

--- content/data-structures/optQueue.h
+++ content/data-structures/optQueue.h
@@ -1,28 +1,33 @@
-template <class T, class F>
+template <class T, class compare=less<T>>
 struct optQueue {
     stack<T> l, r;
-    T infinity;
     T best;
-    F operation;
-    optQueue(T infinity, F operation):
-      best(infinity), operation(operation), infinity(infinity) {}
+    optQueue() {}
     void add(T c) {
       r.push(c);
-      best = operation(best, c);
+      best = r.empty() ? c : compare()(best, c);
+    }
+    bool empty() {
+      return l.empty() && r.empty();
+    }
+    size_t size() {
+      return l.size() + r.size();
     }
     void pop() {
+      assert(!empty());
       if (l.empty()) {
-        best = infinity;
         while (!r.empty()) {
           if (l.empty()) l.push(r.top());
-          else l.push(operation(l.top(), r.top()));
+          else l.push(compare()(l.top(), r.top()));
           r.pop();
         }
         if (!l.empty()) l.pop();
       } else l.pop();
     }
     T get() {
+      assert(!empty());
       if (l.empty()) return best;
-      return operation(l.top(), best);
+      if (r.empty()) return l.top();
+      return compare()(l.top(), best);
     }
 };
