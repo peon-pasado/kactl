c13176f0f97890f1af5b7c0935f182954c26e0ba
racsosabe <racso_galvan@hotmail.com>
2021-01-20 10:02:42 -0500

    Added Booth and updated Suffix Array

---
+++ content/strings/BoothMinimalRotation.h
@@ -0,0 +1,21 @@
+int booth(string s){
+	s += s;
+	int n = s.size();
+	vector<int> pi(n, 0);
+	int k = 0;
+	for(int i = 1; i < n; i++){
+		int p = pi[i - k - 1];
+		while(p > 0 and s[i] != s[k + p]){
+			if(s[i] < s[k + p]) k = i - p;
+			p = pi[p - 1];
+		}
+		if(s[i] != s[k + p]){
+			if(s[i] < s[k]) k = i;
+			pi[i - k] = 0;
+		}
+		else{
+			pi[i - k] = p + 1;
+		}
+	}
+	return k;
+}
--- content/strings/SuffixArray2.cpp
+++ content/strings/SuffixArray2.cpp
@@ -1,85 +1,60 @@
 // Lexicographically minimum suffix array & LCP array construction
 // SA: O(nlogn), LCP: O(n)
 
-vector<int> suffix_array(vector<int> &s, int char_bound = -1) {
+vector<int> suffix_array(vector<int> &s) {
 	int n = s.size();
 	vector<int> a(n);
-	if (n == 0) {
-		return a;
+	vector<int> mapeo(n);
+	iota(a.begin(), a.end(), 0);
+	sort(a.begin(), a.end(), [&] (int i, int j){
+		return s[i] < s[j];
+	});
+	mapeo[a[0]] = 0;
+	for(int i = 1; i < n; i++){
+		mapeo[a[i]] = mapeo[a[i - 1]] + (s[a[i - 1]] != s[a[i]]);
 	}
-	if (char_bound != -1) {
-		vector<int> aux(char_bound, 0);
-		for (int i = 0; i < n; i++) {
-			aux[s[i]]++;
-		}
-		int sum = 0;
-		for (int i = 0; i < char_bound; i++) {
-			int add = aux[i];
-			aux[i] = sum;
-			sum += add;
-		}
-		for (int i = 0; i < n; i++) {
-			a[aux[s[i]]++] = i;
-		}
-	} else {
-		iota(a.begin(), a.end(), 0);
-		sort(a.begin(), a.end(), [&](int i, int j) { return s[i] < s[j]; });
-	}
+	int len = 1;
+	vector<int> head(n);
+	vector<int> new_mapeo(n);
 	vector<int> sorted_by_second(n);
-	vector<int> ptr_group(n);
-	vector<int> new_group(n);
-	vector<int> group(n);
-	group[a[0]] = 0;
-	for (int i = 1; i < n; i++) {
-		group[a[i]] = group[a[i - 1]] + (!(s[a[i]] == s[a[i - 1]]));
-	}
-	int step = 1;
-	while (step < n) {
-		int at = 0;
+	while(len < n){
+		for(int i = 0; i < n; i++) sorted_by_second[i] = (a[i] + n - len) % n;
+		for(int i = n - 1; i >= 0; i--) head[mapeo[a[i]]] = i;
 		for(int i = 0; i < n; i++){
-			sorted_by_second[at++] = a[i] - step < 0? a[i] - step + n : a[i] - step;
-		}
-		for (int i = n - 1; i >= 0; i--) {
-			ptr_group[group[a[i]]] = i;
-		}
-		for (int i = 0; i < n; i++) {
 			int x = sorted_by_second[i];
-			a[ptr_group[group[x]]++] = x;
+			a[head[mapeo[x]]++] = x;
 		}
-		new_group[a[0]] = 0;
-		for (int i = 1; i < n; i++) {
-			if (group[a[i]] != group[a[i - 1]]) {
-				new_group[a[i]] = new_group[a[i - 1]] + 1;
-			} else {
-				int pre = (a[i - 1] + step >= n ? group[a[i - 1] + step - n] : group[a[i - 1] + step]);
-				int cur = (a[i] + step >= n ? group[a[i] + step - n] : group[a[i] + step]);
-				new_group[a[i]] = new_group[a[i - 1]] + (pre != cur);
+		new_mapeo[a[0]] = 0;
+		for(int i = 1; i < n; i++){
+			if(mapeo[a[i]] != mapeo[a[i - 1]]){
+				new_mapeo[a[i]] = new_mapeo[a[i - 1]] + 1;
+			}
+			else{
+				int pre = mapeo[(a[i - 1] + len) % n];
+				int cur = mapeo[(a[i] + len) % n];
+				new_mapeo[a[i]] = new_mapeo[a[i - 1]] + (pre != cur);
 			}
 		}
-		swap(group, new_group);
-		step <<= 1;
+		swap(mapeo, new_mapeo);
+		len <<= 1;
 	}
-	sort(a.begin(),a.end(),[&](int i, int j){
-		if(group[i] == group[j]) return i < j;
-		return group[i] < group[j];
-	});
 	return a;
 }
 
 vector<int> lcp_construction(vector<int> &s, vector<int> &a){
 	int n = s.size();
 	vector<int> rank(n, 0);
-	for(int i=0; i<n; i++){
+	for(int i = 0; i < n; i++){
 		rank[a[i]] = i;
 	}
 	int k = 0;
-	vector<int> lcp(n-1,0);
-	for(int i=0; i<n; i++){
-		if(rank[i] == n-1){
+	vector<int> lcp(n - 1, 0);
+	for(int i = 0; i < n; i++){
+		if(rank[i] == n - 1){
 			k = 0;
 			continue;
 		}
-		int j = a[rank[i]+1];
+		int j = a[rank[i] + 1];
 		while(i + k < n and j + k < n and s[i + k] == s[j + k]) k += 1;
 		lcp[rank[i]] = k;
 		if(k > 0) k -= 1;
