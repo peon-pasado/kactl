5907600c9f1a74b15c312b6e52436c0b60d35716
Miguel Alessandro Mini H <45865868+peon-pasado@users.noreply.github.com>
2021-08-09 22:25:14 -0500

    bfs tranpose

---
+++ content/graph/bfs_transpose.cpp
@@ -0,0 +1,35 @@
+/**
+ * @Author Miguel Mini
+ * @name BFS-TRANPOSE
+ * @input: a graph g with nodes between 0 and sz(g)-1.
+ * @output: number of components and nodes in each component.
+**/
+
+
+int bfs_tranpose(vector<vector<int>>& g, vector<vector<int>>& components) {
+	vector<int> inB(g.size(), -1);
+	vector<int> B(g.size());
+	iota(B.begin(), B.end(), 0);
+	int res = 0;
+	do {
+		queue<int> Q;
+		Q.push(B.back());
+		B.pop_back();
+    components.push_back(vector<int>());
+		//open component
+		while (!Q.empty()) {
+			int q = Q.front(); Q.pop();
+			//q is a component node
+      components.back().push_back(q);
+			for (int u : g[q]) inB[u] = q;
+			int j = 0;
+			for (int i = 0; i < B.size(); ++i)
+				if (inB[B[i]] == q) B[j++] = B[i];
+				else Q.push(B[i]);
+			B.resize(j);
+		}
+		res++;
+		//close component
+	} while (!B.empty());
+	return res;
+}
